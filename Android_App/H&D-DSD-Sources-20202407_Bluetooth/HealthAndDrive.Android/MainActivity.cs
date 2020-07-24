aa97b30583b7f393c7c0aa4038df802a3548433c
Bah Amadou <aobah34@gmail.com>
2020-07-24 11:01:40 +0200

    Fetch repository

---
+++ Android_App/H&D-DSD-Sources-20202407_Bluetooth/HealthAndDrive.Android/MainActivity.cs
@@ -0,0 +1,211 @@
+﻿using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.Content.PM;
+using Android.Net;
+using Android.OS;
+using CarouselView.FormsPlugin.Android;
+using Crossover.Bazarin.Permissions;
+using HealthAndDrive.Droid.Services;
+using HealthAndDrive.Services;
+using Lottie.Forms.Droid;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Android.Provider;
+using Android.Runtime;
+using Prism.Events;
+using HealthAndDrive.Events;
+
+namespace HealthAndDrive.Droid
+{
+    [Activity(Label = "DSD", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait, LaunchMode = LaunchMode.SingleTop)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private const int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469;
+
+        /// <summary>
+        /// Bluetooth enable request code
+        /// </summary>
+        private const int PERMISSION_REQUEST_ENABLE_BLUETOOTH = 2;
+
+        private Intent widgetServiceIntent;
+
+        /// <summary>
+        /// Coarse Location enable permission request
+        /// </summary>
+        private const int PERMISSION_REQUEST_COARSE_LOCATION = 456;
+
+        /// <summary>
+        /// Action Manage Overlay permission request 
+        /// </summary>
+        private const int PERMISSION_ACTION_MANAGE_OVERLAY_REQUEST_CODE = 5469;
+
+
+        /// <summary>
+        /// Widget Background service
+        /// </summary>
+        private Intent serviceWidget;
+
+        /// <summary>
+        /// Measure Service Background
+        /// </summary>
+        private Intent measureService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundle"></param>
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+
+            // Init the carousel view renderer
+            CarouselViewRenderer.Init();
+
+            // Init the animation view renderer
+            AnimationViewRenderer.Init();
+
+            // Check permissions for bluetooth or location
+            CheckPermissions();
+
+            DependencyService.Register<IMeasure, MeasureService>();
+            DependencyService.Register<IFloatingWidgetService, MeasureWidgetService>();
+
+            // Init the background service
+            this.measureService = new Intent(this, typeof(MeasureService));
+            StopService(this.measureService);
+            StartService(this.measureService);
+
+            this.serviceWidget = new Intent(this, typeof(MeasureWidgetService));
+            StopService(this.serviceWidget);
+
+
+            DependencyService.Get<IFloatingWidgetService>().IsWidgetEnabled += ((sender, _) =>
+            {
+                if (!Settings.CanDrawOverlays(this))
+                {
+                    Intent intent = new Intent(Settings.ActionManageOverlayPermission,
+                        Uri.Parse("package:" + PackageName.ToString()));
+                    StartActivityForResult(intent, PERMISSION_ACTION_MANAGE_OVERLAY_REQUEST_CODE);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Called when App started or resumed
+        /// </summary>
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (serviceWidget != null)
+            {
+                StopService(serviceWidget);
+            }
+        }
+
+        /// <summary>
+        /// Called when app isn't focused
+        /// </summary>
+        protected override void OnPause()
+        {
+            base.OnPause();
+            if (Settings.CanDrawOverlays(this) && (DependencyService.Get<IFloatingWidgetService>().IsEnable))
+            {
+                StartService(serviceWidget);
+            }
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            (CrossPermissions.Current as PermissionsImplementation).OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        /// <summary>
+        /// Checks the need permissions and 
+        /// If it's not enabled
+        /// </summary>
+        private void CheckPermissions()
+        {
+            // BLUETOOTH
+            // If bluetooth is not enabled, ask to enable it
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+            if (!bluetoothAdapter.IsEnabled)
+            {
+                var enableBtIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+                StartActivityForResult(enableBtIntent, PERMISSION_REQUEST_ENABLE_BLUETOOTH);
+            }
+
+
+            // COARSE LOCATION
+            // If location is not enabled, ask to enable it
+            //if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            //{
+            //    RequestPermissions(new string[] { Manifest.Permission.AccessCoarseLocation}, PERMISSION_REQUEST_COARSE_LOCATION);
+            //}
+
+        }
+
+        /// <summary>
+        /// Event fired when new intent is executed
+        /// </summary>
+        /// <param name="intent"></param>
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            Intent = intent;
+            NotificationClickedOn(intent);
+        }
+
+        /// <summary>
+        /// Action when the notification is clicked on
+        /// </summary>
+        /// <param name="intent"></param>
+        private void NotificationClickedOn(Intent intent)
+        {
+            var navigationService = (Xamarin.Forms.Application.Current as App).PrismNavigation;
+            Device.BeginInvokeOnMainThread(async () => await navigationService.NavigateAsync("RootPage/PrismNavigationPage/MainTabbedPage?selectedTab=DrivePage"));
+        }
+
+        protected override void OnDestroy()
+        {
+            //ContainerResolve
+            // if(widget)
+            System.Diagnostics.Debug.WriteLine($"OnDestroy raised");
+            
+            base.OnDestroy();
+
+            // Destroy Widget Service
+            if (this.serviceWidget != null)
+            {
+                StopService(serviceWidget);
+            }
+
+            // Destroy Measure Service
+            if (this.measureService != null)
+            {
+                StopService(this.measureService);
+            }
+        }
+    }
+    
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+            containerRegistry.RegisterSingleton<MeasureService>();
+
+        }
+    }
+}
+
---
+++ Android_App/H&D-DSD-Sources-20202407_Bluetooth/HealthAndDrive/Tools/AppSettings.cs
@@ -0,0 +1,190 @@
+﻿
+namespace HealthAndDrive.Tools
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Date and time format
+        /// </summary>
+        public string DATE_AND_TIME_FORMAT = "dd/MM/yyyy HH:mm";
+
+        /// <summary>
+        // For the MVP, there is only one user in local
+        /// </summary>
+        public string USER_ID = "deadbeef-cafe-babe-cafe-babecafebabe";
+
+        /// <summary>
+        /// The default index page for the carousel in the bluetooth page
+        /// </summary>
+        public long BLUETOOTH_INDEX_PAGE_DEFAULT = 0;
+
+        /// <summary>
+        /// The device index page for the carousel in the bluetooth page
+        /// </summary>
+        public long BLUETOOTH_INDEX_PAGE_DEVICE = 1;
+
+        /// <summary>
+        /// Bluetooth scan timeout in seconds
+        /// </summary>
+        public int BLUETOOTH_SCAN_TIMEOUT = 5;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DEVICE_GUID_FORMAT = "00000000-0000-0000-0000-{0}";
+
+        /// <summary>
+        /// Number of milliseconds in a minute
+        /// </summary>
+        public int MILLISECONDS_IN_MINUTE = 60000;
+
+        /// <summary>
+        /// RX Characteristic UUID
+        /// </summary>
+        public string NRF_UART_RX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+        /// <summary>
+        /// Protocol : TX Characteristic UUID
+        /// </summary>
+        public string NRF_UART_TX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+
+        /// <summary>
+        /// Emergency number to call
+        /// </summary>
+        public int EMERGENCY_NUMBER = 112;
+
+        /// <summary>
+        /// TDE
+        /// </summary>
+        public string TDE = "1048198951";
+
+        /// <summary>
+        /// HELP URL 
+        /// </summary>
+        public string HELP_URL = "https://www.healthdrivetechnologies.eu/aide";
+
+        /// <summary>
+        /// WEBSITE URL 
+        /// </summary>
+        public string WEBSITE_URL = "https://www.healthdrivetechnologies.eu/";
+
+        /// <summary>
+        /// Notification interval between two measures in minutes
+        /// </summary>
+        public int MEASURE_NOTIFICATION_INTERVAL = 5;
+
+        /// <summary>
+        /// Glucose Measure Service retry default time in minutes
+        /// </summary>
+        public int MEASURE_SERVICE_RETRY_DEFAULT_TIME = 1;
+
+        /// <summary>
+        /// The maximum of the maximum glucose value
+        /// </summary>
+        public int ALERTS_MAX_OF_MAX_GLUCOSE_VALUE = 220;
+
+        /// <summary>
+        /// The minimum of the maximum glucose value
+        /// </summary>
+        public int ALERTS_MIN_OF_MAX_GLUCOSE_VALUE = 170;
+
+        /// <summary>
+        /// The maximum of the minimum glucose value
+        /// </summary>
+        public int ALERTS_MAX_OF_MIN_GLUCOSE_VALUE = 80;
+
+        /// <summary>
+        /// The minimum of the minimum glucose value
+        /// </summary>
+        public int ALERTS_MIN_OF_MIN_GLUCOSE_VALUE = 60;
+
+        /// <summary>
+        /// MilliMol  per Deciliter constant
+        /// </summary>
+        public static double MMOLL_TO_MGDL = 18.0182;
+
+        /// <summary>
+        /// Milligram to Millimol per liter constant
+        /// </summary>
+        public double MGDL_TO_MMOLL = 1 / MMOLL_TO_MGDL;
+
+        /// <summary>
+        /// Indicates if we deal with MGDL_MEASAURE
+        /// </summary>
+        public bool HANDLE_MGDL_MEASURE = true;
+
+        /// <summary>
+        /// The min value for the medical zone (included)
+        /// </summary>
+        public float MEDICAL_ZONE_MGDL_MIN = 70 ;
+
+        /// <summary>
+        /// The max value for the medical zone (included)
+        /// </summary>
+        public float MEDICAL_ZONE_MGDL_MAX = 180;
+
+        /// <summary>
+        /// Red color forr drive page
+        /// </summary>
+        public string COLOR_RED = "#e01010";
+
+        /// <summary>
+        /// Orange color forr drive page
+        /// </summary>
+        public string COLOR_ORANGE = "#f45b17";
+
+        /// <summary>
+        /// Yellow color forr drive page
+        /// </summary>
+        public string COLOR_YELLOW = "#edad1e";
+
+        /// <summary>
+        /// Green color forr drive page
+        /// </summary>
+        public string COLOR_GREEN = "#62d248";
+
+        /// <summary>
+        /// The export filename for the data in the zone
+        /// </summary>
+        public string CSV_IN_ZONE_FILENAME = "HealthAndDrive.data_inzone.csv";
+
+        /// <summary>
+        /// The export filename for the datas not in the zone
+        /// </summary>
+        public string CSV_NOT_IN_ZONE_FILENAME = "HealthAndDrive.data_not_inzone.csv";
+
+        /// <summary>
+        /// The chart vertical axes max value
+        /// </summary>
+        public int CHART_VERT_AXE_MAX = 455;
+
+        /// <summary>
+        /// The chart vertical axes min value
+        /// </summary>
+        public int CHART_VERT_AXE_MIN = 0;
+
+        /// <summary>
+        /// The alert page name to avoid alert page redirection when
+        /// you're already on the page
+        /// </summary>
+        public string ALERTS_PAGE_NAME = "ALERTES";
+
+        /// <summary>
+        /// The HI value used in drive page and widget
+        /// </summary>
+        public int HI_VALUE = 400;
+
+        /// <summary>
+        /// The LO value used in drive page and widget
+        /// </summary>
+        public int LO_VALUE = 30;
+
+        /// <summary>
+        /// Retry Delay in second
+        /// </summary>
+        public int RetryBluetoothDelay = 30;
+
+
+        
+    }
+}
